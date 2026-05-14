b1097ea02280bd9371285aa79ef5eb4a76948d70
juyterman1000 <fastrunner10090@gmail.com>
2026-05-13 17:47:55 -0700

    chore(packaging): pin homebrew formula to v0.19.2 sdist

--- packaging/homebrew/entroly.rb
+++ packaging/homebrew/entroly.rb
@@ -21,8 +21,8 @@ class Entroly < Formula
 
   desc "Token-saving proxy and context compression engine for AI coding agents"
   homepage "https://entroly.dev"
-  url "https://files.pythonhosted.org/packages/source/e/entroly/entroly-0.19.2.tar.gz"
-  sha256 "REPLACE_WITH_SDIST_SHA256_AT_RELEASE_TIME"
+  url "https://files.pythonhosted.org/packages/98/bf/7c364526a976d3442a543f4b1a5cb68a2a2e324585223a6a8965ecfb7e20/entroly-0.19.2.tar.gz"
+  sha256 "b4ca09f4835c153edaefd7c1f512054154a4807e01e508dac62c5ed40cdb1f30"
   license "Apache-2.0"
   head "https://github.com/juyterman1000/entroly.git", branch: "main"
 
